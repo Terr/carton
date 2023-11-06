9f27846abd7e82e34e242f0736fefa22b6ef9fa8
Terr <git@terr.nl>
2023-11-06 11:53:51 +0100

    Cargo workspace setup

---
+++ crates/carton_bin/src/bin/carton.rs
@@ -0,0 +1,6 @@
+// Copyright 2023 Arjen Verstoep
+// SPDX-License-Identifier: Apache-2.0
+
+fn main() {
+    println!("Hello, World!");
+}
---
+++ crates/libcarton/src/lib.rs
@@ -0,0 +1,2 @@
+// Copyright 2023 Arjen Verstoep
+// SPDX-License-Identifier: Apache-2.0
